e3bd2e4358491885fad19fd65bdbc80454f142ed
mabbason <miles.abbason@gmail.com>
2022-06-03 09:56:07 -0400

    added new favicon and css to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ function Home({ handleClick }) {
 	return (
 		<div className="container-main">
 			<div className="main-box">
-			<h2>Inspect HTTP Requests</h2>
+			<h2><strong>Inspect HTTP Requests</strong></h2>
 			<h3>Snaggl generates a URL that will collect requests made to it and let you inspect them in a human-friendly way.
           Use Snaggl to see what your HTTP client is sending or to inspect and debug webhook requests.</h3>
-			<button type="button" id="createbin_btn" className="button" onClick={handleClick}>+ Create Snaggl Bin</button>
+			<button type="button" id="createbin_btn" className="button" onClick={handleClick}><strong>+ Create Snaggl Bin</strong></button>
 			</div>
 		</div>
 	);
